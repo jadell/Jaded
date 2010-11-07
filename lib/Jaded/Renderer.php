9a6a1fc127b83e1352ba7063e6ec6fec1a0ed9ce
Josh Adell <josh.adell@gmail.com>
2010-11-07 11:39:53 -0500

    Added basic HTML renderer.

---
+++ lib/Jaded/Renderer.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class to handle displaying the results in a response
+ */
+interface Jaded_Renderer
+{
+	//////////////////////////////////////////////////////////////////////
+	// PUBLIC ///////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////
+
+	/**
+	 * Render the view with the given information
+	 * This should actually output the rendered string (and headers, etc.)
+	 * @param string $sTemplate
+	 * @param array  $aData
+	 * @param array  $aHeaders
+	 */
+	public function render($sTemplate, $aData, $aHeaders);
+}
+?>
